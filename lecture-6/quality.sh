6fdfdebe1f72ecf60dd3ba91596709b3b8c49fd0
nahid18 <nahidpatwary50@email.com>
2022-03-13 05:18:27 +0600

    updated readme

--- lecture-6/quality.sh
+++ lecture-6/quality.sh
@@ -4,5 +4,5 @@ ASSEMBLED="megahit_out/final.contigs.fa"
 REF_FASTA="data/MN908947.3.fasta"
 REF_GFF="data/ASM985889v3.100.gff3"
 
-
 quast.py -o quast_out $ASSEMBLED -r $REF_FASTA -g $REF_GFF -t 4 --space-efficient
+
